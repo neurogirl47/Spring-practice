9b718025c1c51a9569209b612ec035a1eeae86c6
neurogirl47 <udochi.okeke@gmail.com>
2022-06-15 20:36:06 -0400

    Tested with track coach

--- spring-demo/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
+++ spring-demo/spring-demo-one/src/com/luv2code/springdemo/TrackCoach.java
@@ -1,6 +1,13 @@
 package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
+	
+	private FortuneService fortuneService;
+
+	
+	public TrackCoach(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
@@ -10,8 +17,7 @@ public class TrackCoach implements Coach {
 
 	@Override
 	public String getDailyFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Just Do It: " +fortuneService.getFortune();
 	}
 
 }
