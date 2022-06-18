ae988f12fa019eb435b498deabb036143cf318fa
neurogirl47 <udochi.okeke@gmail.com>
2022-06-18 19:07:43 -0400

    practiced using qualifier on randomFortuneService implementation

--- spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/RandomFortuneService.java
@@ -1,14 +1,30 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class RandomFortuneService implements FortuneService {
-
+	
+	//create an array of strings
+	private String[] data = {
+			"Beware the wolf in sheep's clothing",
+			"Diligence is the mother of good luck",
+			"The journey is the reward"
+	};
+	
+	//create a random number generator
+	private Random myRandom = new Random();
+	
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		// pick a random string from the array
+		int index = myRandom.nextInt(data.length);
+		
+		String theFortune = data[index];
+		
+		return theFortune;
 	}
 
 }
